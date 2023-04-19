de42ae5e647c0d27b273ab109e26e2d1c0940069
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2023-04-19 18:39:56 +0530

    learn

---
+++ readME.txt
@@ -0,0 +1,28 @@
+1.. create app name "learn-mui"
+(install - npm install @material-ui/core) --> this is not installed , so rather than this install below package.
+2.. install "npm install @mui/material @emotion/react @emotion/styled"
+3.. Remove previous code from "App.js" & simply add "message that you want". 
+4.. Check on browser "npm start"
+
+//** Button
+-lots of variant & lots of button type in mui 
+- mui.com-> go component -> Button
+1. use Button --> <Button></Button> 
+2. property --> 
+        ~ variant - will change layout & it can change the background as well as supporter
+                ->text(normal), outlined, contained
+        ~ color - primary, info, secondary, warning, success, error
+        ~ size - small, medium, large
+        ~ border
+        ~ disabled 
+        ~ disableElevation
+        ~ for link --> href="#text-buttons"
+3. icon with button 
+       -for icon --> " npm install @mui/icons-material "
+4. click on button
+5. change color on button click
+
+//** ButtonGroup
+1. use
+2. properties
+3. orientation - vertical, horizonatal
--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Box, IconButton, Icon } from '@mui/material';
+// import {Delete} from '@mui/icons-material';
 
 function App() {
+  const [colors, setColor] = useState('secondary')
+  const [disable, setDisable] = useState(false)
+  function funButton() {
+    // alert("color Changed!!");
+    // setColor('warning')
+    setDisable(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React MUI</h1>
+      <Box sx={{ '& button': { m: 1 } }}>
+        <Button variant="text" color='secondary' size='small'>Text</Button>
+        <Button variant="contained" color='success' size='medium'>Contained</Button>
+        <Button variant="outlined" color='error' size='large'>Outlined</Button>
+        <Button color='primary'>Primary</Button>
+        {/* <Button color='action'>Action</Button> */}
+        <Button color='warning'>Primary</Button>
+        <Button disabled>Disabled</Button>
+        <Button href="#text-buttons">Link</Button>
+        <Button variant="outlined" disableElevation>Disable elevation</Button>
+        <Button variant='contained' component="label"> Upload Button <input hidden accept='image/*' type='file' /></Button>
+        <IconButton color="primary" component="label">
+          <input hidden accept="image/*" aria-label="upload picture" type="file" />
+          <Icon>PhotoCamera</Icon>
+        </IconButton>
+      </Box>
+      <br /><br /><br />
+      {/* 1. want to change color on the click ---> use useState */}
+      {/* 2. want to disabled on button click use -- another state */}
+      <Button
+        variant='contained' color={colors} disabled={disable}
+        onClick={() => funButton()}
+        // startIcon = {Delete}
+      >Click Me
+      </Button>
     </div>
   );
 }
